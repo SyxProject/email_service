5ef5a2e035529ffe66e05bb5898b47c9c1f9481d
SebasLaptop <sebasnio17@gmail.com>
2025-11-19 15:17:04 -0500

    fix port biding

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
+const express = require('express')
 const consume = require('./rabbitmq/consumer')
 
-console.log("Email service iniciado. Esperando eventos...")
+const app = express()
+const port = process.env.PORT || 4000
+
 consume()
+
+app.listen(port, () => {
+  console.log(`Event Service escuchando en puerto ${port}`)
+  startConsumer().catch(console.error)
+})
